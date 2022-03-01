81413c8c065e237f7f5486d319b738ee762e035a
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-01 19:49:59 +0330

    a

--- src/A_1/A_1.java
+++ src/A_1/A_1.java
@@ -1,31 +1,38 @@
 package A_1;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class A_1 {
     public static void main(String[] args) {
         String[] words = {"Amir", "Hatef", "Mehran", "Mojtaba",
-                "Mohammad", "Ali" ,"Davood", "Reza", "Mohsen"};
+                "Mohammad", "Ali", "Davood", "Reza", "Mohsen"};
         List<String> wordList = Arrays.asList(words);
 
-        Map<Integer,List<String>> group =
+        Map<Integer, List<String>> group =
                 wordList.stream()
                         .collect(
-                          Collectors.groupingBy(
-                                  String::length
-                          )
+                                Collectors.groupingBy(
+                                        String::length
+                                )
                         );
+
         System.out.println(group);
 
-        Map<String,Long> groupBySize =
+
+        for (Map.Entry<Integer, List<String>> a : group.entrySet()){
+            ;
+            System.out.println(a+" "+a.getValue().stream()
+                    .count());
+        }
+
+        Map<Integer, Long> groupBySize =
                 wordList.stream()
                         .collect(
-                                Collectors.groupingBy(Function.identity(), Collectors.counting())
+                                Collectors.groupingBy(String::length, Collectors.counting())
                         );
         System.out.println(groupBySize);
 
